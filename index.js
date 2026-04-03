fe466348e9589c9cbc2d97d22c0211dba5a37cd9
Денис Рыпунов <rypunov2005@bk.ru>
2026-04-03 17:06:31 +0300

    Add task copying

--- index.js
+++ index.js
@@ -37,6 +37,14 @@ function createItem(item) {
 		saveTasks(items)
 	})
 
+	duplicateButton.addEventListener('click', () => {
+		const itemName = textElement.textContent;
+		const newItem = createItem(itemName);
+		listElement.prepend(newItem);
+		const items = getTasksFromDOM();
+		saveTasks(items);
+	})
+
 	return clone;
 }
 
